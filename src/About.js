c6652b0e6dd342e0cb00741c61e1e6809bc94fd0
SarmadMoeen <74090144+Sarmad108@users.noreply.github.com>
2023-11-21 14:53:12 +0500

    Final Commit

---
+++ src/About.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./About.css";
+import aboutImg from "./img/about.jpg";
+function About() {
+  //  Up To Top Btn
+    window.addEventListener("scroll", function(){
+      const upToTop = document.querySelector("a.bottom__to__top");
+       upToTop.classList.toggle("active", window.scrollY > 0)
+    });
+  return (
+    <div className="about component__space" id="About">
+      <div className="container">
+        <div className="row">
+          <div className="col__2">
+            <img src={aboutImg} alt="" className="about__img" />
+          </div>
+          <div className="col__2">
+            <h1 className="about__heading">About Me</h1>
+            <div className="about__meta">
+              <p className="about__text p__color">
+              I am a Highly motivated and detail-oriented software engineer with experience in a variety of
+programming languages and a passion for problem-solving. Strong skills in programming, data
+structures and algorithms, and software development methodologies.
+              </p>
+              <p className="about__text p__color">
+              To obtain a challenging position in a high-quality Software Engineering environment
+where my resourceful experience and academic skills will add value to organizational
+operations
+              </p>
+              <div className="about__button d__flex align__items__center">
+                <a href="#">
+                  <button className="about btn pointer">Download Cv</button>
+                </a>
+                <a href="#">
+                  <button className="about btn pointer">Hire Me</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* UP TO TOP BTN */}
+      <div className="up__to__top__btn">
+        <a href="#" className="bottom__to__top">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-up white"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+          />
+        </svg>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Project.js
@@ -0,0 +1,134 @@
+import React from "react";
+import "./Project.css";
+import Project1 from './img/portfolio-1.jpg';
+import Project2 from './img/portfolio-2.jpg';
+import Project3 from './img/portfolio-3.jpg';
+import Project4 from './img/portfolio-4.jpg';
+
+function Project() {
+  return (
+    <div className="project component__space" id="Portfolio">
+      <div className="heading">
+        <h1 className="heading">My Latest Project</h1>
+        <p className="heading p__color">
+         Many of the Projects I Had done with the Organziations
+        </p>
+        <p className="heading p__color">
+          So Some of Are:
+        </p>
+      </div>
+       <div className="container">
+           <div className="row">
+             
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project1} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">LivTech</h5>
+                     <h4 className="project__text">Developed on ASP.NET</h4>
+                     <a href="#" className="project__btn">Support as L3</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project2} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Evv-Portal</h5>
+                     <h4 className="project__text">Developed on ASP.NET</h4>
+                     <a href="#" className="project__btn">Support as L3</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project3} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">SwiftCart</h5>
+                     <h4 className="project__text">Devloper On MERN Stack</h4>
+                     <a href="#" className="project__btn">Team Player as FUll Stack</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project4} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">BuiltTop</h5>
+                     <h4 className="project__text">Developer on ASP.NET and React JS</h4>
+                     <a href="#" className="project__btn"></a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project3} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Real Estate Website</h5>
+                     <h4 className="project__text">Developed on MERN Stack</h4>
+                     <a href="#" className="project__btn">worked as Full Stack</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project4} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Fitness Freaks</h5>
+                     <h4 className="project__text">Developed on Full Stack</h4>
+                     <a href="#" className="project__btn">Worked as Full Stack Developer</a>
+                     </div>
+                 </div>
+             </div>
+             
+             <div className="view__more__btn d__flex align__items__center justify__content__center pxy__30">
+             <button className="view__more pointer btn">View more</button>
+             </div>
+
+           </div>
+       </div>
+    </div>
+  );
+}
+
+export default Project;
